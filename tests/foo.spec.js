ef2127c43ecc8bc0c9e89f902500d60e19b110c4
Dylan Hassinger <dylan@nd2a.com>
2021-11-18 10:25:56 -0600

    added DOMAIN env to test

--- tests/foo.spec.js
+++ tests/foo.spec.js
@@ -1,7 +1,10 @@
 const { test, expect } = require('@playwright/test');
 
+// set domain to env or localhost
+const domain = process.env['DOMAIN'] || 'http://localhost:3000';
+
 test('basic test', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto(domain);
   const title = page.locator('h1');
   await expect(title).toHaveText('Welcome to Next.js!');
 });
